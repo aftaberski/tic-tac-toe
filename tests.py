7c20818d282ba96c2953eb31639e8d6f5309520d
Anna Taberski <aftaberski@gmail.com>
2016-05-01 08:42:31 -0400

    Remove double negatives from tests

--- tests.py
+++ tests.py
@@ -8,35 +8,40 @@ class TestGame(TestCase):
         self.player2 = Player("Marvin The Martian", "O")
         self.game = Game(self.player1, self.player2)
 
-    def test_who_won(self):
+    def test_is_winner(self):
         # Test left to right diagonal
         self.game.board.game_board = [["X", "O", ""],
                                     ["O", "X", ""],
                                     ["", "", "X"]]
-        self.assertEqual(False, self.game.is_not_winner())
+        self.assertEqual(True, self.game.is_winner())
 
         # Test vertical win
         self.game.board.game_board = [["", "O", "X"],
                                     ["O", "X", "X"],
                                     ["", "O", "X"]]
-        self.assertEqual(False, self.game.is_not_winner())
+        self.assertEqual(True, self.game.is_winner())
 
         # Test horizontal win
         self.game.board.game_board = [["O", "O", "O"],
                                     ["O", "X", "X"],
                                     ["", "X", "X"]]
-        self.assertEqual(False, self.game.is_not_winner())
+        self.assertEqual(True, self.game.is_winner())
+
+        self.game.board.game_board = [["O", "X", "O"],
+                                    ["O", "O", "X"],
+                                    ["", "X", "X"]]
+        self.assertEqual(False, self.game.is_winner())
 
     def test_is_finished(self):
         self.game.board.game_board = [["X", "O", ""],
                                       ["O", "X", ""],
                                       ["", "O", ""]]
-        self.assertEqual(True, self.game.is_not_finished())
+        self.assertEqual(False, self.game.is_finished())
 
         self.game.board.game_board = [["X", "O", "O"],
                                       ["O", "X", "X"],
                                       ["O", "O", "O"]]
-        self.assertEqual(False, self.game.is_not_finished())
+        self.assertEqual(True, self.game.is_finished())
 
 if __name__ == '__main__':
     main()
